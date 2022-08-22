403bdcc3d84848a6044a0ba9f8bcb8514b971efb
hosamation <mohammad.hosam@gmail.com>
2022-08-22 22:14:14 +0200

    Add Video & Playlist Downloader

---
+++ youtube_downloader.py
@@ -0,0 +1,27 @@
+from pytube import YouTube
+
+# video_link = YouTube(input("Please enter the video URL: "))
+video_link = YouTube("Paste URL Video Here")
+
+def finish():
+  print("Download Done!")
+
+video_link.streams.get_highest_resolution().download(output_path= input("Enter full path of the folder"))
+# video_link.streams.get_highest_resolution().download(output_path= "/home/hosamation/Downloads")   # Example
+video_link.register_on_complete_callback(finish())
+
+# Download Youtube Playlist
+# ------------------------- #
+
+from pytube import Playlist
+
+# playlist = Playlist(input("Please enter the playlist URL: "))
+playlist = Playlist("Paste URL Playlist Here")
+
+# for video in playlist.videos[0:2]: # To Download specific videos from the playlist [Start : End + 1]
+for video in playlist.videos:
+  video.streams.get_highest_resolution().download(output_path="Enter full path of the folder")
+  # video.streams.get_highest_resolution().download(output_path="/home/hosamation/Downloads")   # Example
+  video.register_on_complete_callback(finish())
+
+###################### To Avoiding any bug Comment What you don't use ######################
