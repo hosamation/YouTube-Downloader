b4129de82dcebf68ffb96911c8d1dedf44901f18
hosamation <mohammad.hosam@gmail.com>
2022-08-22 22:20:27 +0200

    create a branch to download video

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -9,19 +9,3 @@ def finish():
 video_link.streams.get_highest_resolution().download(output_path= input("Enter full path of the folder"))
 # video_link.streams.get_highest_resolution().download(output_path= "/home/hosamation/Downloads")   # Example
 video_link.register_on_complete_callback(finish())
-
-# Download Youtube Playlist
-# ------------------------- #
-
-from pytube import Playlist
-
-# playlist = Playlist(input("Please enter the playlist URL: "))
-playlist = Playlist("Paste URL Playlist Here")
-
-# for video in playlist.videos[0:2]: # To Download specific videos from the playlist [Start : End + 1]
-for video in playlist.videos:
-  video.streams.get_highest_resolution().download(output_path="Enter full path of the folder")
-  # video.streams.get_highest_resolution().download(output_path="/home/hosamation/Downloads")   # Example
-  video.register_on_complete_callback(finish())
-
-###################### To Avoiding any bug Comment What you don't use ######################
