34da1d6f6f99678fb5eb0dd5e5b854634ec9c710
hosamation <mohammad.hosam@gmail.com>
2023-03-28 02:42:11 +0200

    declare counter and optimize the loop in playlist

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -15,13 +15,17 @@ video_link.register_on_complete_callback(finish())
 
 from pytube import Playlist
 
-# playlist = Playlist(input("Please enter the playlist URL: "))
+# playlist = Playlist(input("Please enter the playlist URL: ")) # Input
 playlist = Playlist("Paste URL Playlist Here")
 
-# for video in playlist.videos[0:2]: # To Download specific videos from the playlist [Start : End + 1]
+counter = 0
+# for video in playlist.videos[175:180]: # To Download specific videos from the playlist [Start : End + 1]
+  
 for video in playlist.videos:
-  video.streams.get_highest_resolution().download(output_path="Enter full path of the folder")
+  # video.streams.get_highest_resolution().download(output_path="Enter full path of the folder")
   # video.streams.get_highest_resolution().download(output_path="/home/hosamation/Downloads")   # Example
-  video.register_on_complete_callback(finish())
+  # video.streams.get_highest_resolution().download(output_path=input("Enter full path of the folder: ")) # Input
+  counter+=1
+  print(f"{counter} - {video.title} Successfully Downloaded.")
 
 ###################### To Avoiding any bug Comment What you don't use ######################
