ede4c258cf3730ce1d865f3eba942a48cdd4fae8
hosamation <mohammad.hosam@gmail.com>
2025-04-27 01:56:19 +0300

    add pytubefix

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,11 +1,9 @@
-from pytube import YouTube
+from pytubefix import YouTube
 
 # video_link = YouTube(input("Please enter the video URL: "))
 video_link = YouTube("Paste URL Video Here")
 
-def finish():
-  print("Download Done!")
 
 video_link.streams.get_highest_resolution().download(output_path= input("Enter full path of the folder"))
 # video_link.streams.get_highest_resolution().download(output_path= "/home/hosamation/Downloads")   # Example
-video_link.register_on_complete_callback(finish())
+print(f"{video_link.title} Successfully Downloaded.")
