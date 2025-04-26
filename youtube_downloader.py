2fc9a974cbccaa9d9dfb79e9d8f398baaa08adce
hosamation <mohammad.hosam@gmail.com>
2025-04-27 02:27:26 +0300

    enhanced

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,14 +1,12 @@
 from pytubefix import Playlist
 
-# playlist = Playlist(input("Please enter the playlist URL: ")) # Input
-playlist = Playlist("Paste URL Playlist Here")
+playlist = Playlist(input("Please enter the playlist URL: ")) # Input
+# playlist = Playlist("Paste URL Playlist Here")
 
 counter = 0
 # for video in playlist.videos[175:180]: # To Download specific videos from the playlist [Start : End + 1]
   
 for video in playlist.videos:
-  # video.streams.get_highest_resolution().download(output_path="Enter full path of the folder")
-  # video.streams.get_highest_resolution().download(output_path="/home/hosamation/Downloads")   # Example
-  # video.streams.get_highest_resolution().download(output_path=input("Enter full path of the folder: ")) # Input
+  video.streams.get_highest_resolution().download(output_path=input("Enter full path of the folder: ")) # Input
   counter+=1
   print(f"{counter} - {video.title} Successfully Downloaded.")
